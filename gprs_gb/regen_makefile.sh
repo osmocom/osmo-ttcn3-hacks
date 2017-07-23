6e594f217ad5a95aac75f91081915444bc4d23f7
Harald Welte <laforge@gnumonks.org>
2017-07-23 16:19:35 +0200

    gprs_gb: Build stack of NS and BSSGP emulation from test component
    We're already getting the NS link up and fail at the BSSGP RESET
    procedure at this point.  The problem is erroneous encoding of the
    message.

--- gprs_gb/regen_makefile.sh
+++ gprs_gb/regen_makefile.sh
@@ -5,5 +5,5 @@ FILES="*.ttcn BSSGP_Helper.cc IPL4asp_PT.cc IPL4asp_discovery.cc TCCConversion.c
 ttcn3_makefilegen -f Test.ttcn $FILES
 sed -i -e 's/# TTCN3_DIR = /TTCN3_DIR = \/usr/' Makefile
 sed -i -e 's/LDFLAGS = /LDFLAGS = -L \/usr\/lib\/titan `pkg-config --libs libnetfilter_conntrack`/' Makefile
-sed -i -e 's/TTCN3_LIB = ttcn3-parallel/TTCN3_LIB = ttcn3/' Makefile
+#sed -i -e 's/TTCN3_LIB = ttcn3-parallel/TTCN3_LIB = ttcn3/' Makefile
 sed -i -e 's/CPPFLAGS = -D$(PLATFORM) -I$(TTCN3_DIR)\/include/CPPFLAGS = -D$(PLATFORM) -I$(TTCN3_DIR)\/include -I\/usr\/include\/titan/' Makefile
