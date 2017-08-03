627c7c7dfa9f0cdc3cb48989f1588adbcaa139f5
Harald Welte <laforge@gnumonks.org>
2017-08-03 18:17:02 +0200

    Revert "GTP: Attempt to use UDP_Testport instead of IPL4asp"
    This reverts commit a967f3f50edd02aa8aaae1044d162758715ff040.

--- ggsn_tests/gen_links.sh
+++ ggsn_tests/gen_links.sh
@@ -23,8 +23,8 @@ DIR=$BASEDIR/titan.TestPorts.Common_Components.Socket-API/src
 FILES="Socket_API_Definitions.ttcn"
 gen_links $DIR $FILES
 
-DIR=$BASEDIR/titan.TestPorts.UDPasp/src
-FILES="UDPasp_PT.cc  UDPasp_PT.hh  UDPasp_PortType.ttcn  UDPasp_Types.ttcn"
+DIR=$BASEDIR/titan.TestPorts.IPL4asp/src
+FILES="IPL4asp_Functions.ttcn  IPL4asp_PT.cc  IPL4asp_PT.hh IPL4asp_PortType.ttcn  IPL4asp_Types.ttcn  IPL4asp_discovery.cc IPL4asp_protocol_L234.hh"
 gen_links $DIR $FILES
 
 DIR=../GTP_v13.5.0_CNL113843_LATEST/src
