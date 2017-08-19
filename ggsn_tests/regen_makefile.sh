8b4123f67fd1487ed4fa7621bad80227333a16f6
Harald Welte <laforge@gnumonks.org>
2017-08-19 15:17:14 +0200

    GGSN_Tests: Enable junit-xml generation

--- ggsn_tests/regen_makefile.sh
+++ ggsn_tests/regen_makefile.sh
@@ -2,8 +2,8 @@
 
 FILES="*.ttcn IPL4asp_PT.cc  IPL4asp_discovery.cc  TCCConversion.cc  TCCInterface.cc GTPC_EncDec.cc GTPU_EncDec.cc GTP_CodecPort_CtrlFunctDef.cc ICMPv6_EncDec.cc IP_EncDec.cc Native_FunctionDefs.cc"
 
-ttcn3_makefilegen -f GGSN_Tests.ttcn $FILES
+ttcn3_makefilegen -l -f GGSN_Tests.ttcn $FILES
 sed -i -e 's/# TTCN3_DIR = /TTCN3_DIR = \/usr/' Makefile
-sed -i -e 's/LDFLAGS = /LDFLAGS = -L \/usr\/lib\/titan/' Makefile
+sed -i -e 's/LDFLAGS = /LDFLAGS = -L \/usr\/lib\/titan /' Makefile
 #sed -i -e 's/TTCN3_LIB = ttcn3-parallel/TTCN3_LIB = ttcn3/' Makefile
 sed -i -e 's/CPPFLAGS = -D$(PLATFORM) -I$(TTCN3_DIR)\/include/CPPFLAGS = -D$(PLATFORM) -I$(TTCN3_DIR)\/include -I\/usr\/include\/titan/' Makefile
---
+++ ggsn_tests/start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+LD_LIBRARY_PATH=.:/usr/lib/titan ttcn3_start GGSN_Tests ./GGSN_Tests.cfg
