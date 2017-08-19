4d7722900a041ff74d70c34e8d2ecabfe8b7b153
Harald Welte <laforge@gnumonks.org>
2017-08-19 15:27:10 +0200

    generalize 'start.sh' script for executing synamically linked testsuite

--- ggsn_tests/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-LD_LIBRARY_PATH=.:/usr/lib/titan ttcn3_start GGSN_Tests ./GGSN_Tests.cfg
---
+++ start-testsuite.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Helper script to starte a TITAN-generated test suite, supporting
+# dynamically linked suites to ensure JUNIT generation is possible.
+
+if [ $# -lt 1 ]; then
+	echo "You have to specify the test suite name"
+	echo "Syntax example: $0 osmo-ttcn3-hacks/ggsn_tests/GGSN_Test ./GGSN_Test.cfg"
+	exit 1
+fi
+
+SUITE=$1
+CFG=`basename $SUITE`.cfg
+if [ $# -gt 1 ]; then
+	CFG=$2
+fi
+
+LD_LIBRARY_PATH=`dirname $SUITE`:/usr/lib/titan ttcn3_start $SUITE $CFG
