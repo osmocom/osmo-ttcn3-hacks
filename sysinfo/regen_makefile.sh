0b7545dff13394fcaac2a9f904fe6858bca7d661
Harald Welte <laforge@gnumonks.org>
2017-08-20 22:59:13 +0200

    sysinfo: build dynamic to enable use of junit-xml logfile plugin
    also remove libnetfilter-conntrack linkage, whcih was a copy+paste
    error.

--- sysinfo/regen_makefile.sh
+++ sysinfo/regen_makefile.sh
@@ -2,8 +2,8 @@
 
 FILES="*.ttcn IPL4asp_PT.cc IPL4asp_discovery.cc TCCConversion.cc TCCInterface.cc IPL4_GSMTAP_CtrlFunctDef.cc TELNETasp_PT.cc"
 
-ttcn3_makefilegen -f Test.ttcn $FILES
+ttcn3_makefilegen -l -f Test.ttcn $FILES
 sed -i -e 's/# TTCN3_DIR = /TTCN3_DIR = \/usr/' Makefile
-sed -i -e 's/LDFLAGS = /LDFLAGS = -L \/usr\/lib\/titan `pkg-config --libs libnetfilter_conntrack`/' Makefile
+sed -i -e 's/LDFLAGS = /LDFLAGS = -L \/usr\/lib\/titan /' Makefile
 sed -i -e 's/TTCN3_LIB = ttcn3-parallel/TTCN3_LIB = ttcn3/' Makefile
 sed -i -e 's/CPPFLAGS = -D$(PLATFORM) -I$(TTCN3_DIR)\/include/CPPFLAGS = -D$(PLATFORM) -I$(TTCN3_DIR)\/include -I\/usr\/include\/titan/' Makefile
