cda8b9add143ccf7af145287efb5d5b43cd49861
Harald Welte <laforge@gnumonks.org>
2017-11-19 01:47:40 +0100

    mgw: Use dynamic runtime to allow use with log output plugins

--- mgw/regen_makefile.sh
+++ mgw/regen_makefile.sh
@@ -2,8 +2,8 @@
 
 FILES="*.ttcn SDP_EncDec.cc *.c MGCP_CodecPort_CtrlFunctDef.cc IPL4asp_PT.cc IPL4asp_discovery.cc TCCConversion.cc TCCInterface.cc RTP_EncDec.cc RTP_CodecPort_CtrlFunctDef.cc"
 
-ttcn3_makefilegen -f MGCP_Test.ttcn $FILES
+ttcn3_makefilegen -l -f MGCP_Test.ttcn $FILES
 sed -i -e 's/# TTCN3_DIR = /TTCN3_DIR = \/usr/' Makefile
-sed -i -e 's/LDFLAGS = /LDFLAGS = -L \/usr\/lib\/titan/' Makefile
+sed -i -e 's/LDFLAGS = /LDFLAGS = -L \/usr\/lib\/titan /' Makefile
 #sed -i -e 's/TTCN3_LIB = ttcn3-parallel/TTCN3_LIB = ttcn3/' Makefile
 sed -i -e 's/CPPFLAGS = -D$(PLATFORM) -I$(TTCN3_DIR)\/include/CPPFLAGS = -D$(PLATFORM) -I$(TTCN3_DIR)\/include -I\/usr\/include\/titan/' Makefile
