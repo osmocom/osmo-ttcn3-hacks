5aa1803ef971b31bf208c172e006954c11ab71de
Harald Welte <laforge@gnumonks.org>
2017-11-25 02:05:04 +0100

    Move MGCP_CodecPort to library directory

--- mgw/gen_links.sh
+++ mgw/gen_links.sh
@@ -37,5 +37,5 @@ FILES="RTP_EncDec.cc RTP_Types.ttcn"
 gen_links $DIR $FILES
 
 DIR=../library
-FILES="General_Types.ttcn Osmocom_Types.ttcn MGCP_Types.ttcn MGCP_Templates.ttcn"
+FILES="General_Types.ttcn Osmocom_Types.ttcn MGCP_Types.ttcn MGCP_Templates.ttcn MGCP_CodecPort.ttcn MGCP_CodecPort_CtrlFunct.ttcn MGCP_CodecPort_CtrlFunctDef.cc"
 gen_links $DIR $FILES
