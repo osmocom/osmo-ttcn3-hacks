26595451b33ff8a07d7dc2f267952ef6211ed7fe
Harald Welte <laforge@gnumonks.org>
2017-11-29 11:46:24 +0100

    IPA_CodecPort: Wrap f_IPL4_setGetMsgLen function

--- library/IPA_CodecPort_CtrlFunctDef.cc
+++ library/IPA_CodecPort_CtrlFunctDef.cc
@@ -51,6 +51,16 @@ namespace IPA__CodecPort__CtrlFunct {
   {
     return f__IPL4__PROVIDER__getUserData(portRef, connId, userData);
   }
-  
+
+  void f__IPL4__setGetMsgLen(
+    IPA__CodecPort::IPA__CODEC__PT& portRef,
+    const IPL4asp__Types::ConnectionId& connId,
+    Socket__API__Definitions::f__getMsgLen& f,
+    const Socket__API__Definitions::ro__integer& msgLenArgs)
+  {
+    return f__IPL4__PROVIDER__setGetMsgLen(portRef, connId, f, msgLenArgs);
+  }
+
+
 }
 
