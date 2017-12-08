140714d89d953a5a0e716460302ce9cd8976630d
Harald Welte <laforge@gnumonks.org>
2017-12-08 14:03:28 +0100

    regen_makefile.sh: Add Regex's for TITAN 6.3.0
    The TITAN 6.3.0 makefile generator needs some new regular expressions
    for patching the generated makefile to work on Debian

--- lapdm/regen_makefile.sh
+++ lapdm/regen_makefile.sh
@@ -7,3 +7,7 @@ sed -i -e 's/# TTCN3_DIR = /TTCN3_DIR = \/usr/' Makefile
 sed -i -e 's/LDFLAGS = /LDFLAGS = -L \/usr\/lib\/titan `pkg-config --libs libnetfilter_conntrack`/' Makefile
 #sed -i -e 's/TTCN3_LIB = ttcn3-parallel/TTCN3_LIB = ttcn3/' Makefile
 sed -i -e 's/CPPFLAGS = -D$(PLATFORM) -I$(TTCN3_DIR)\/include/CPPFLAGS = -D$(PLATFORM) -I$(TTCN3_DIR)\/include -I\/usr\/include\/titan/' Makefile
+
+# for TITAN 6.3.0
+sed -i -e 's/TTCN3_DIR = /TTCN3_DIR = \/usr/' Makefile
+sed -i -e 's/\/bin\/compiler/\/bin\/ttcn3_compiler/' Makefile
