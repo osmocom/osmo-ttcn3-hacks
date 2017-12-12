9fbe93f5329bbd153a1f7dd3c70e0a54a211feae
Harald Welte <laforge@gnumonks.org>
2017-12-13 00:39:44 +0100

    regen_makefile.sh: Fix TITAN 6.1.0 support
    When introducing the rewriting for 6.3.0 we broke the 6.1.0 support.
    Now the result should build on Debian with both 6.1.0 and 6.3.0
    
    Change-Id: I263a6abd2d9eb15ce5778ef8dbfcdac2a228b77d

--- lapdm/regen_makefile.sh
+++ lapdm/regen_makefile.sh
@@ -9,5 +9,5 @@ sed -i -e 's/LDFLAGS = /LDFLAGS = -L \/usr\/lib\/titan `pkg-config --libs libnet
 sed -i -e 's/CPPFLAGS = -D$(PLATFORM) -I$(TTCN3_DIR)\/include/CPPFLAGS = -D$(PLATFORM) -I$(TTCN3_DIR)\/include -I\/usr\/include\/titan/' Makefile
 
 # for TITAN 6.3.0
-sed -i -e 's/TTCN3_DIR = /TTCN3_DIR = \/usr/' Makefile
+sed -i -e 's/TTCN3_DIR = $/TTCN3_DIR = \/usr/' Makefile
 sed -i -e 's/\/bin\/compiler/\/bin\/ttcn3_compiler/' Makefile
