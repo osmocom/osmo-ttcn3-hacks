0e23a8fb46ace06a0dc51b1e477b9de8abc70464
Harald Welte <laforge@gnumonks.org>
2017-12-12 16:47:48 +0100

    gprs_gb: Fix compilation of tests
    It's unsure if the tests do anything useful.   At least they build again
    
    The fundamental problem is some clashes/overlap between our own
    definitions for certain protocols and those that Ericsson released
    later.  Ideally we should use Ericsson whenever possible and resort on
    our own ones only as a fall-back for those that don't exist, but
    significant development has been done before Ericsson released their
    codecs, so it's TBD for the future.

--- gprs_gb/gen_links.sh
+++ gprs_gb/gen_links.sh
@@ -36,5 +36,5 @@ FILES="UD_PT.cc  UD_PT.hh  UD_PortType.ttcn  UD_Types.ttcn"
 gen_links $DIR $FILES
 
 DIR=../library
-FILES="General_Types.ttcn GSM_Types.ttcn GSM_RR_Types.ttcn Osmocom_Types.ttcn RLCMAC_Types.ttcn RLCMAC_EncDec.cc L1CTL_Types.ttcn L1CTL_PortType.ttcn LAPDm_RAW_PT.ttcn LAPDm_Types.ttcn"
+FILES="General_Types.ttcn GSM_Types.ttcn GSM_RR_Types.ttcn Osmocom_Types.ttcn RLCMAC_Types.ttcn RLCMAC_CSN1_Types.ttcn RLCMAC_EncDec.cc L1CTL_Types.ttcn L1CTL_PortType.ttcn LAPDm_RAW_PT.ttcn LAPDm_Types.ttcn"
 gen_links $DIR $FILES
