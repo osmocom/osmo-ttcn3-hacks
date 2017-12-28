2e1a1da6748aab607ec75e104909048e36ff8412
Harald Welte <laforge@gnumonks.org>
2017-12-28 23:11:35 +0100

    bsc-test: Fix compilation after IuUP support was introduced
    Ever since I46de7fb9c324654275a27aa5f8c1be70340e3229 we had problems
    linking the object files together due to a missing object
    
    Change-Id: I5d8c37f924b3a2c8b28cace4da2add1fadd0151a

--- bsc/regen_makefile.sh
+++ bsc/regen_makefile.sh
@@ -2,6 +2,6 @@
 
 MAIN=BSC_Tests.ttcn
 
-FILES="*.ttcn IPA_CodecPort_CtrlFunctDef.cc IPL4asp_PT.cc IPL4asp_discovery.cc TCCConversion.cc TCCInterface.cc SCTPasp_PT.cc RTP_EncDec.cc SDP_EncDec.cc RTP_CodecPort_CtrlFunctDef.cc MGCP_CodecPort_CtrlFunctDef.cc *.c"
+FILES="*.ttcn IPA_CodecPort_CtrlFunctDef.cc IPL4asp_PT.cc IPL4asp_discovery.cc TCCConversion.cc TCCInterface.cc SCTPasp_PT.cc RTP_EncDec.cc SDP_EncDec.cc RTP_CodecPort_CtrlFunctDef.cc MGCP_CodecPort_CtrlFunctDef.cc IuUP_EncDec.cc *.c"
 
 ../regen-makefile.sh $MAIN $FILES
