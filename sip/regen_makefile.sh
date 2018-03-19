afec471557785b4e4c0d379bd3ff0903a732b7cd
Harald Welte <laforge@gnumonks.org>
2018-03-19 22:52:17 +0100

    sip: Add SIP_Emulation and first osmo-sip-connector test case
    Change-Id: Ifd47b0d48c609b4a678ea47aa7f89f5c12e6c0d2

---
+++ sip/regen_makefile.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+FILES="*.ttcn *.ttcnpp TCCConversion.cc TCCInterface.cc UD_PT.cc MNCC_EncDec.cc IPL4asp_PT.cc
+IPL4asp_discovery.cc SDP_EncDec.cc RTP_EncDec.cc IPA_CodecPort_CtrlFunctDef.cc RTP_CodecPort_CtrlFunctDef.cc TELNETasp_PT.cc Native_FunctionDefs.cc SIPmsg_PT.cc *.c "
+
+export CPPFLAGS_TTCN3=""
+
+../regen-makefile.sh SIP_Tests.ttcn $FILES
