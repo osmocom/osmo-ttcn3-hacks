0c6f34cf8f53e6b1ed4b04846f871bc18ccf33db
Daniel Willmann <dwillmann@sysmocom.de>
2018-10-24 17:26:41 +0200

    Rename IPA_Test->BSCNAT_Tests and get proper cfg files
    Change-Id: I937d2aa1abfcb302dfb529ac8be75dcd269c46e2

--- bsc-nat/regen_makefile.sh
+++ bsc-nat/regen_makefile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MAIN=IPA_Test.ttcn
+MAIN=BSCNAT_Tests.ttcn
 
 FILES="*.ttcn *.ttcnpp SCCP_EncDec.cc IPA_CodecPort_CtrlFunctDef.cc IPL4asp_PT.cc IPL4asp_discovery.cc TCCConversion.cc TCCInterface.cc RTP_EncDec.cc SDP_EncDec.cc *.c MGCP_CodecPort_CtrlFunctDef.cc"
 
