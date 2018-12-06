4263c52bd4a8d42af2e484eac49500e68bce96c0
Harald Welte <laforge@gnumonks.org>
2018-12-06 11:56:27 +0100

    WIP: MSC_Tests: Add SGs testcases
    This extens MSC_Tests.ttcn with an initial set of SGs interface test
    cases for RESET, LU, DETACH, PAGING, SMS and CSFB procedures
    
    In particular the following testcases are added:
    
    - TC_sgsap_reset: isolated reset procedure test
    - TC_sgsap_lu: isolated location update with TMSI realloc
    - TC_sgsap_lu_imsi_reject: location update, reject case
    - TC_sgsap_lu_and_nothing: location update with failed TMSI realloc
    - TC_sgsap_expl_imsi_det_eps: detach from EPS serveces
    - TC_sgsap_expl_imsi_det_noneps: detach from non-EPS services
    - TC_sgsap_paging_rej: isolated paging, reject case
    - TC_sgsap_paging_subscr_rej: isolated paging, subscr rejects call
    - TC_sgsap_paging_ue_unr: isolated paging, ue is unreachable
    - TC_sgsap_paging_and_nothing: page, but don't respond
    - TC_sgsap_paging_and_lu: check paging followed by an LU
    - TC_sgsap_mt_sms: mobile terminated SMS through SGs Interface
    - TC_sgsap_mo_sms: mobile originated SMS through SGs Interface
    - TC_sgsap_mt_sms_and_nothing: trigger SMS, but don't respond to paging
    - TC_sgsap_mt_sms_and_reject: trigger SMS, but reject paging
    - TC_sgsap_unexp_ud: Send unexpected unitdata (SGs Association: NULL)
    - TC_sgsap_unsol_ud: Send unsolicited unitdata (subscriber not in VLR)
    - TC_bssap_lu_sgsap_lu_and_mt_call: LU on 2G, LU on SGs and CSFB call
    - TC_sgsap_lu_and_mt_call: LU on SGs, and CSFB call
    
    Change-Id: I38543c35a9e74cea276e58d1d7ef01ef07ffe858
    Depends: osmo-msc I73359925fc1ca72b33a1466e6ac41307f2f0b11d
    Related: OS#3645

--- msc/gen_links.sh
+++ msc/gen_links.sh
@@ -94,7 +94,7 @@ FILES+="RTP_CodecPort.ttcn RTP_CodecPort_CtrlFunctDef.cc "
 FILES+="MGCP_CodecPort.ttcn MGCP_CodecPort_CtrlFunctDef.cc "
 FILES+="SMPP_CodecPort.ttcn SMPP_CodecPort_CtrlFunct.ttcn SMPP_CodecPort_CtrlFunctDef.cc SMPP_Emulation.ttcn SMPP_Templates.ttcn "
 FILES+="SS_Templates.ttcn SCCP_Templates.ttcn USSD_Helpers.ttcn "
-FILES+="SGsAP_Templates.ttcn "
+FILES+="SGsAP_Templates.ttcn SGsAP_CodecPort.ttcn SGsAP_CodecPort_CtrlFunct.ttcn SGsAP_CodecPort_CtrlFunctDef.cc SGsAP_Emulation.ttcn DNS_Helpers.ttcn "
 gen_links $DIR $FILES
 
 ignore_pp_results
