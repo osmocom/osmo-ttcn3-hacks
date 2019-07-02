35498115655dacac5d7f443c00ebc76c72d587f0
Harald Welte <laforge@gnumonks.org>
2019-07-02 14:26:39 +0800

    library: Add S1AP CodecPort/Emulation
    Change-Id: I9bfba3ab2a3830e590b203c44c03b9c9383fff99

--- mme/regen_makefile.sh
+++ mme/regen_makefile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-FILES="*.ttcn IPL4asp_PT.cc  IPL4asp_discovery.cc  Native_FunctionDefs.cc SGsAP_CodecPort_CtrlFunctDef.cc  TCCConversion.cc  TCCEncoding.cc  TCCInterface.cc  TELNETasp_PT.cc "
+FILES="*.ttcn *.asn IPL4asp_PT.cc  IPL4asp_discovery.cc  Native_FunctionDefs.cc SGsAP_CodecPort_CtrlFunctDef.cc  TCCConversion.cc  TCCEncoding.cc  TCCInterface.cc  TELNETasp_PT.cc S1AP_EncDec.cc "
 
 export CPPFLAGS_TTCN3=""
 
