42d745c694c3d279610cbbfbe5206bd4f96a729c
Harald Welte <laforge@gnumonks.org>
2019-08-01 09:54:40 +0200

    WIP: Initial skeleton of CBC tests
    Change-Id: I38286e8a3dd0f39bd25f631dcbb3ff4f8d4c221f

---
+++ cbc/regen_makefile.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+FILES="*.ttcn TCCConversion.cc TCCInterface.cc TCCEncoding.cc IPL4asp_PT.cc IPL4asp_discovery.cc TELNETasp_PT.cc Native_FunctionDefs.cc SCTPasp_PT.cc CBSP_CodecPort_CtrlFunctdef.cc "
+#FILES+="*.ttcnpp *.c *.asn"
+
+export CPPFLAGS_TTCN3=""
+
+../regen-makefile.sh CBC_Tests.ttcn $FILES
+
+sed -i -e 's/^LINUX_LIBS = -lxml2/LINUX_LIBS = -lxml2 -lfftranscode/' Makefile
