0e04c53c632d8242022be126969bdcd35318aee0
Harald Welte <laforge@gnumonks.org>
2019-09-23 06:10:03 +0200

    cbc: Add SABP Selftests
    Change-Id: I97c31dd3f9ef700fc40074fe7963329d5206af5f

--- cbc/gen_links.sh
+++ cbc/gen_links.sh
@@ -37,6 +37,7 @@ DIR=../library
 FILES="Misc_Helpers.ttcn General_Types.ttcn GSM_Types.ttcn Osmocom_Types.ttcn Native_Functions.ttcn Native_FunctionDefs.cc "
 FILES+="CBSP_Types.ttcn CBSP_Templates.ttcn "
 FILES+="CBSP_CodecPort.ttcn CBSP_CodecPort_CtrlFunct.ttcn CBSP_CodecPort_CtrlFunctdef.cc CBSP_Adapter.ttcn "
+FILES+="SABP_CodecPort.ttcn SABP_CodecPort_CtrlFunct.ttcn SABP_CodecPort_CtrlFunctDef.cc SABP_Adapter.ttcn "
 gen_links $DIR $FILES
 
 ignore_pp_results
