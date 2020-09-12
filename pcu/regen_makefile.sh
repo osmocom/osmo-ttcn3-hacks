93331e736bbe9181ca2337bd031a525ace532f54
Harald Welte <laforge@osmocom.org>
2020-09-12 20:51:05 +0200

    BSSGP_Emulation: Make L3 (SM/GMM) decoding a compile-time option
    This allows us to build low-level tests that don't require all of L3.
    
    Change-Id: I7ef7e93e2cd1d37e7e4a62947ca8dfde30d93b95

--- pcu/regen_makefile.sh
+++ pcu/regen_makefile.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-FILES="*.ttcn BSSGP_EncDec.cc IPL4asp_PT.cc IPL4asp_discovery.cc TCCConversion.cc TCCInterface.cc NS_CodecPort_CtrlFunctDef.cc UD_PT.cc RLCMAC_EncDec.cc LLC_EncDec.cc TELNETasp_PT.cc Native_FunctionDefs.cc"
+FILES="*.ttcn *.ttcnpp BSSGP_EncDec.cc IPL4asp_PT.cc IPL4asp_discovery.cc TCCConversion.cc TCCInterface.cc NS_CodecPort_CtrlFunctDef.cc UD_PT.cc RLCMAC_EncDec.cc LLC_EncDec.cc TELNETasp_PT.cc Native_FunctionDefs.cc"
+
+export CPPFLAGS_TTCN3="-DBSSGP_EM_L3"
 
 ../regen-makefile.sh PCU_Tests.ttcn $FILES
