2dd230a63c81a616f98dffb7a32fc5b70820375a
Harald Welte <laforge@osmocom.org>
2021-01-26 15:44:46 +0100

    fr-net: Fix regen_makefile.sh
    regen-makefile.sh should not include generated c++ code.
    
    Change-Id: I0fecfb0d3c33f8d87923d4843fee718916001978

--- fr-net/regen_makefile.sh
+++ fr-net/regen_makefile.sh
@@ -3,7 +3,7 @@
 FILES="*.ttcn *.ttcnpp BSSGP_EncDec.cc IPL4asp_PT.cc IPL4asp_discovery.cc TCCConversion.cc TCCInterface.cc "
 FILES+="AF_PACKET_PT.cc "
 FILES+="Native_FunctionDefs.cc "
-FILES+="LLC_EncDec.cc LLC_Types.cc TELNETasp_PT.cc "
+FILES+="LLC_EncDec.cc TELNETasp_PT.cc "
 
 export CPPFLAGS_TTCN3="-DNS_EMULATION_FR"
 
