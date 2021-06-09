b5c880e1af35edff6d7fe3bb7524c33d4973e255
Pau Espin Pedrol <pespin@sysmocom.de>
2021-06-09 16:19:26 +0200

    bts: Support passing PCUIF messages over IPA multiplex
    Related: SYS#5303
    Change-Id: I3cd0988cb654f1c5816d1c4717255a5e802d7925

--- bts/regen_makefile.sh
+++ bts/regen_makefile.sh
@@ -2,6 +2,6 @@
 
 FILES="*.ttcn *.ttcnpp IPA_CodecPort_CtrlFunctDef.cc IPL4asp_PT.cc IPL4asp_discovery.cc TCCConversion.cc TCCInterface.cc UD_PT.cc RLCMAC_EncDec.cc Native_FunctionDefs.cc TRXC_CodecPort_CtrlFunctDef.cc L1CTL_PortType_CtrlFunctDef.cc TELNETasp_PT.cc"
 
-export CPPFLAGS_TTCN3="-DIPA_EMULATION_RSL -DIPA_EMULATION_OML -DIPA_EMULATION_CTRL"
+export CPPFLAGS_TTCN3="-DIPA_EMULATION_RSL -DIPA_EMULATION_OML -DIPA_EMULATION_CTRL -DIPA_EMULATION_OSMO_PCU"
 
 ../regen-makefile.sh BTS_Tests.ttcn $FILES
