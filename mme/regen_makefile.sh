f92ccad16bb0f3d0fe5cdd325d57204ba1dc36d4
Oliver Smith <osmith@sysmocom.de>
2021-10-12 13:01:26 +0200

    mme: link against gnutls
    Fix for:
    /usr/bin/ld: key_derivation.so: undefined reference to `gnutls_hmac_fast'
    
    Related: OS#5252
    Change-Id: Iacddc22ecda187e250db9cfb1ca1d1d341ce78f4

--- mme/regen_makefile.sh
+++ mme/regen_makefile.sh
@@ -26,4 +26,4 @@ export CPPFLAGS_TTCN3="
 
 ../regen-makefile.sh MME_Tests.ttcn $FILES
 
-sed -i -e 's/^LINUX_LIBS = -lxml2 -lsctp/LINUX_LIBS = -lxml2 -lsctp -lfftranscode/' Makefile
+sed -i -e 's/^LINUX_LIBS = -lxml2 -lsctp/LINUX_LIBS = -lxml2 -lsctp -lfftranscode -lgnutls/' Makefile
