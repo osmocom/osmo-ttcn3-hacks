c8f7d93849c00eddd99ffa58e301192733c5d2d9
Daniel Willmann <dwillmann@sysmocom.de>
2022-01-05 09:12:34 +0100

    Add hnbgw test
    Change-Id: I0a2fb795aec83337eda8d9972e6ff264ead61076

---
+++ hnbgw/regen_makefile.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+NAME=HNBGW_Tests
+
+FILES="
+	*.asn
+	*.c
+	*.ttcn
+	*.ttcnpp
+	IPA_CodecPort_CtrlFunctDef.cc
+	IPL4asp_PT.cc
+	IPL4asp_discovery.cc
+	IuUP_EncDec.cc
+	Iuh_CodecPort_CtrlFunctDef.cc
+	Native_FunctionDefs.cc
+	RTP_CodecPort_CtrlFunctDef.cc
+	RTP_EncDec.cc
+	SCTPasp_PT.cc
+	SDP_EncDec.cc
+	StatsD_CodecPort_CtrlFunctdef.cc
+	TCCConversion.cc
+	TCCEncoding.cc
+	TCCInterface.cc
+	TELNETasp_PT.cc
+	HNBAP_EncDec.cc
+	RUA_EncDec.cc
+	RANAP_EncDec.cc
+	MGCP_CodecPort_CtrlFunctDef.cc
+	UD_PT.cc
+"
+
+export CPPFLAGS_TTCN3="
+	-DIPA_EMULATION_CTRL
+	-DIPA_EMULATION_MGCP
+	-DIPA_EMULATION_SCCP
+	-DRAN_EMULATION_RANAP
+	-DRAN_EMULATION_CTRL
+	-DRAN_EMULATION_MGCP
+	-DUSE_MTP3_DISTRIBUTOR
+"
+
+../regen-makefile.sh -e $NAME $FILES
+
+sed -i -e 's/^LINUX_LIBS = -lxml2 -lsctp/LINUX_LIBS = -lxml2 -lsctp -lfftranscode/' Makefile
