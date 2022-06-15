7b23060141d76508b3d7314572296f522fae97ae
Pau Espin Pedrol <pespin@sysmocom.de>
2022-06-15 16:28:20 +0200

    SABP: Add regen_makefile.sh
    Other similar libraries also have it.
    
    Change-Id: I74b31c041b254713b95821ff166640465ccd1409

---
+++ library/sabp/regen_makefile.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+FILES="*.asn *.ttcn SABP_EncDec.cc"
+
+../../regen-makefile.sh $FILES
+
+sed -i -e '/^CPPFLAGS/ s/$/ `pkg-config --cflags libfftranscode`/' Makefile
+sed -i -e '/^LDFLAGS/ s/$/ `pkg-config --libs libfftranscode`/' Makefile
+sed -i -e '/^LINUX_LIBS/ s/$/ `pkg-config --libs libfftranscode`/' Makefile
