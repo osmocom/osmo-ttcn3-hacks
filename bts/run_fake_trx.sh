054f13e70bc13b9ab9d3105217d82ec150e53f26
Alexander Couzens <lynxis@fe80.eu>
2022-12-19 21:48:46 +0100

    osmo-bts: add simple run_fake_trx.sh
    Most BTS_Tests require to run fake_trx. Add a simple
    run_fake_trx.sh script when running these test without
    docker.
    
    Change-Id: Ie3a68931bd52f55570409bb35962cebbfd58d168

---
+++ bts/run_fake_trx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Most BTS_Tests require to have fake_trx running.
+# fake trx is part of osmo-trx
+FAKE_TRX_DIR="${FAKE_TRX_DIR:-../../osmo-trx/osmocom-bb/src/target/trx_toolkit}"
+
+if [ -n "$FAKE_TRX_DIR" ] ; then
+    cd "$FAKE_TRX_DIR"
+fi
+
+exec ./fake_trx.py  --trx TRX1@127.0.0.1:5700/1 --trx TRX2@127.0.0.1:5700/2 --trx TRX3@127.0.0.1:5700/3
