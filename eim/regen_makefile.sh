773d5824faecbd797d8fe6fc2768c1290fb807cf
Philipp Maier <pmaier@sysmocom.de>
2024-03-11 14:57:17 +0100

    eIM_Tests: add testsuite for an eIM
    This patch adds a testsuite for an eIM server component. It simulates
    the SM-DP+ server backend and the IPAd client while it operates the
    REST API of the eIM server.
    
    Change-Id: Iba72ee00d10aba7831feedadacd943bf943de53e
    Related: SYS#6824

---
+++ eim/regen_makefile.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+NAME=eIM_Tests
+
+FILES="
+	*.ttcn
+	*.asn
+	Abstract_Socket.cc
+	HTTPmsg_MessageLen_Function.cc
+	HTTPmsg_PT.cc
+	IPL4asp_PT.cc
+	IPL4asp_discovery.cc
+	Native_FunctionDefs.cc
+	TCCConversion.cc
+	TCCInterface.cc
+	SGP32Definitions_EncDec.cc
+	RSPDefinitions_EncDec.cc
+	PKIX1Explicit88_EncDec.cc
+	PKIX1Implicit88_EncDec.cc
+	PIPEasp_PT.cc
+"
+../regen-makefile.sh eIM_Tests.ttcn $FILES
+
+# required for forkpty(3) used by PIPEasp
+sed -i -e '/^LINUX_LIBS/ s/$/ -lutil/' Makefile
