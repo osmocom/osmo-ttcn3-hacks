5badb2191d916c26138daf66679c68925f7d01ab
Pau Espin Pedrol <pespin@sysmocom.de>
2024-05-15 14:36:53 +0200

    remsim: Move PIPEasp generic helper code to library/PIPEasp_Templates.ttcn
    This way code can be used and extended for other testsuites.
    
    Change-Id: I4a5ededbd958c87537f84155092b6a6cd715865a

--- remsim/gen_links.sh
+++ remsim/gen_links.sh
@@ -39,6 +39,7 @@ gen_links $DIR $FILES
 
 DIR=../library
 FILES="Misc_Helpers.ttcn General_Types.ttcn Osmocom_VTY_Functions.ttcn Osmocom_Types.ttcn "
+FILES+="PIPEasp_Templates.ttcn "
 FILES+="IPA_Types.ttcn IPA_CodecPort.ttcn IPA_CodecPort_CtrlFunct.ttcn IPA_CodecPort_CtrlFunctDef.cc IPA_Emulation.ttcnpp IPA_CodecPort.ttcn " #RSL_Types.ttcn RSL_Emulation.ttcn "
 FILES+="Osmocom_CTRL_Types.ttcn Osmocom_CTRL_Functions.ttcn Osmocom_CTRL_Adapter.ttcn  "
 FILES+="Native_Functions.ttcn Native_FunctionDefs.cc "
