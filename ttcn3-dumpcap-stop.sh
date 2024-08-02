162c1cd8484d615d3c0b1224f6af34b032f3f05d
Pau Espin Pedrol <pespin@sysmocom.de>
2024-08-02 17:53:30 +0200

    ttcn3-(tcp)dump(pcap)-stop.sh: store multiple runs of same test into different pcap file
    This is useful when testing the behavior of a SUT when the same scenario
    is run twice, for instance to debug a crash in the SUT.
    In this setup, it is definetly interesting to have stored pcaps of the
    several times the scenario is run.
    
    Change-Id: Ib68fc4df1f5a105b4b2c904d2d2003c621a2ad31

--- ttcn3-dumpcap-stop.sh
+++ ttcn3-dumpcap-stop.sh
@@ -48,4 +48,14 @@ done
 kill_rm_pidfile "$PIDFILE_PCAP"
 kill_rm_pidfile "$PIDFILE_NETCAT"
 
-gzip -f "$TTCN3_PCAP_PATH/$TESTCASE.pcapng"
+# Add a numeral suffix to subsequent runs of the same test:
+PCAP_FILENAME=$TTCN3_PCAP_PATH/$TESTCASE.pcap
+if [ -f "$TTCN3_PCAP_PATH/$TESTCASE.pcap.gz" ]; then
+       i=1
+       while [ -f "$TTCN3_PCAP_PATH/$TESTCASE.$i.pcap.gz" ];
+               do i=$((i+1))
+       done
+       mv "$PCAP_FILENAME" "$TTCN3_PCAP_PATH/$TESTCASE.$i.pcap"
+       PCAP_FILENAME="$TTCN3_PCAP_PATH/$TESTCASE.$i.pcap"
+fi
+gzip -f "$PCAP_FILENAME"
