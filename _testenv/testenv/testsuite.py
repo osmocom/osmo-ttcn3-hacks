osmocom/osmo-ttcn3-hacks

commit 95231624f12c299019c0d6064b5d4d3004dad093

testenv: fix testsuite.run() not passing env variables
Running start-testsuite.sh in a clean env fails on systems with non-
standard TTCN3_BIN_DIR and TITAN_LIBRARY_PATH paths.  This is the
case for Arch Linux and the eclipse-titan AUR package.  Let's call
cmd.generate_env() to allow overriding these variables.

Change-Id: I95c3cfe41ee230927bb5777631372597cbd3c051

_testenv/testenv/testsuite.py
@@ -162,9 +162,8 @@ def run(cfg):
     start_testsuite = os.path.join(ttcn3_hacks_dir, "start-testsuite.sh")
     suite = os.path.join(ttcn3_hacks_dir, testenv.args.testsuite, section_data["program"])
 
-    env = {
-        "TTCN3_PCAP_PATH": os.path.join(testenv.testdir.testdir, "testsuite"),
-    }
+    pcap_path = os.path.join(testenv.testdir.testdir, "testsuite")
+    env = testenv.cmd.generate_env({ "TTCN3_PCAP_PATH" : pcap_path })
 
     cmd = [start_testsuite, suite, section_data["config"]]
 

commit 5e4a5f5382d0312e96314b1b29f1cd847d353e5f

testenv: Fix testsuite.run() passing too many envvars to podman
A recent commit added some code in testenv to pass all environment to the
testsuite being run. However, we don't want that happening when running
the testsuite under podman, since then we'd be polluting the env inside
the container with the env from the host running the container.

This fixes running testenv with podman from an Archlinux host having
some TTCN_* extra variables used by testsuite scripts to find ttcn3
binaries & libs under /opt/eclipse-titan/.

Fixes: 95231624f12c299019c0d6064b5d4d3004dad093
Change-Id: Ia8fca37b878323cd188035004b587c124598376e

_testenv/testenv/testsuite.py
@@ -162,8 +162,10 @@ def run(cfg):
     start_testsuite = os.path.join(ttcn3_hacks_dir, "start-testsuite.sh")
     suite = os.path.join(ttcn3_hacks_dir, testenv.args.testsuite, section_data["program"])
 
-    pcap_path = os.path.join(testenv.testdir.testdir, "testsuite")
-    env = testenv.cmd.generate_env({ "TTCN3_PCAP_PATH" : pcap_path })
+    env = {
+        "TTCN3_PCAP_PATH": os.path.join(testenv.testdir.testdir, "testsuite"),
+    }
+    env = testenv.cmd.generate_env(env, testenv.args.podman)
 
     cmd = [start_testsuite, suite, section_data["config"]]
 