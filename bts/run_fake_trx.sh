fa4995e8de0c32242f9ffeed3666b8f53cabebbe
Oliver Smith <osmith@sysmocom.de>
2024-10-18 14:08:52 +0200

    bts: add initial testenv.cfg
    As usually, imported from docker-playground and adjusted to run on the
    loopback device.
    
    Related: OS#6638
    Change-Id: Id9ed3780d166c5f6736a006f466dc5f0dded6279

--- bts/run_fake_trx.sh
+++ bts/run_fake_trx.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-
 # Most BTS_Tests require to have fake_trx running.
 # fake trx is part of osmo-trx
+
+if [ "$TESTENV_BINARY_REPO" = 1 ]; then
+	# testenv with --binary-repo -> osmocom-bb cloned separately
+	FAKE_TRX_DIR="$TESTENV_GIT_DIR"/osmocom-bb/src/target/trx_toolkit
+elif [ -n "$TESTENV_SRC_DIR" ]; then
+	# testenv without --binary-repo -> osmocom-bb cloned via osmo-dev
+	FAKE_TRX_DIR="$TESTENV_SRC_DIR"/osmocom-bb/src/target/trx_toolkit
+fi
+
 FAKE_TRX_DIR="${FAKE_TRX_DIR:-../../osmo-trx/osmocom-bb/src/target/trx_toolkit}"
 
 cd "$FAKE_TRX_DIR"
