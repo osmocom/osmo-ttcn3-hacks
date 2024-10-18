cdb36a10758e0de0cbc584d0b30eb77954e4fe89
Pau Espin Pedrol <pespin@sysmocom.de>
2024-10-18 18:53:31 +0200

    ggsn: Introduce test TC_pdp4(6)_act_deact_mtu
    This tests so far only test retrieval of MTU over PCO, which is only
    used for IPv4 APNs.
    When IPv6 is in used, it is expected to be retrieved over IPv6 SLAAC RA.
    Such tests will be done in a follow-up patch once the related procedure
    is implemented in osmo-ggsn.
    
    Related: OS#6298
    Related: SYS#7122
    Change-Id: I2ceb76a3a07b44f69937c9f5a040482320bca377

--- ggsn_tests/gen_links.sh
+++ ggsn_tests/gen_links.sh
@@ -58,7 +58,7 @@ FILES="DIAMETER_EncDec.cc"
 gen_links $DIR $FILES
 
 DIR=../library
-FILES="Misc_Helpers.ttcn General_Types.ttcn GSM_Types.ttcn Osmocom_Types.ttcn Native_Functions.ttcn Native_FunctionDefs.cc IPCP_Types.ttcn IPCP_Templates.ttcn PAP_Types.ttcn "
+FILES="Misc_Helpers.ttcn General_Types.ttcn GSM_Types.ttcn Osmocom_Types.ttcn Native_Functions.ttcn Native_FunctionDefs.cc PCO_Types.ttcn IPCP_Types.ttcn IPCP_Templates.ttcn PAP_Types.ttcn "
 FILES+="GTPv1C_CodecPort.ttcn GTPv1C_CodecPort_CtrlFunct.ttcn GTPv1C_CodecPort_CtrlFunctDef.cc GTPv1C_Templates.ttcn Osmocom_Gb_Types.ttcn "
 FILES+="GTPv1U_CodecPort.ttcn GTPv1U_CodecPort_CtrlFunct.ttcn GTPv1U_CodecPort_CtrlFunctDef.cc GTPv1U_Templates.ttcn "
 FILES+="DIAMETER_Types.ttcn DIAMETER_CodecPort.ttcn DIAMETER_CodecPort_CtrlFunct.ttcn DIAMETER_CodecPort_CtrlFunctDef.cc DIAMETER_Emulation.ttcn "
