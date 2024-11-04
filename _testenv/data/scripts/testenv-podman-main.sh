2766aba0c4b02034e17c5b065585956d86d727f1
Oliver Smith <osmith@sysmocom.de>
2024-11-04 15:26:15 +0100

    testenv: podman-main: set watchdog timer to 60s
    Increase the timer from 10s to 60s, as with 10s I see jobs failing with:
    
      ERROR: /tmp/watchdog was not created, exiting
    
    In theory 10s should already be enough, my guess is that if a jenkins
    node is currently under a lot of load then the feed command may take
    several seconds and so we hit the previous timer. Even if this is not
    the cause, I think it is good to rule it out.
    
    Exiting after 60s if the jenkins job was (manually / with connection
    loss) aborted is still relatively quick.
    
    Related: OS#6607
    Change-Id: Ide7a9d5bc395106cdaa47cec3aa746a978674c9e

--- _testenv/data/scripts/testenv-podman-main.sh
+++ _testenv/data/scripts/testenv-podman-main.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 # Simple watchdog script that exits if either:
-# * testenv doesn't create /tmp/watchdog every 10s
+# * testenv doesn't create /tmp/watchdog every 60s
 # * 4 hours have passed
-# This ensures the podman container stops a few seconds after a jenkins job was
+# This ensures the podman container stops a soon after a jenkins job was
 # aborted, or if a test is stuck in a loop for hours.
 
 echo "Running testenv-podman-main.sh"
@@ -10,7 +10,7 @@ echo "Running testenv-podman-main.sh"
 stop_time=$(($(date +%s) + 3600 * 4))
 
 while [ $(date +%s) -lt $stop_time ]; do
-	sleep 10
+	sleep 60
 
 	if ! [ -e /tmp/watchdog ]; then
 		echo "ERROR: /tmp/watchdog was not created, exiting"
