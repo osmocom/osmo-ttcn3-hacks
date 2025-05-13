ab4ad451b2b9626b5e0cb3cc62ae53f0c5460ee3
Eric Wild <ewild@sysmocom.de>
2025-05-13 12:59:50 +0200

    unfug mit encdec, aber baut

--- smdpp2/regen_makefile.sh
+++ smdpp2/regen_makefile.sh
@@ -19,8 +19,9 @@ FILES="
 	PKIX1Implicit88_EncDec.cc
 	PIPEasp_PT.cc
 	JSON_EncDec.cc
+	foo.cc
 "
 ../regen-makefile.sh smdpp2_Tests.ttcn smdpp2_json.ttcn3 smdpp2_es9p_types.ttcn3 $FILES
 
 # required for forkpty(3) used by PIPEasp
-sed -i -e '/^LINUX_LIBS/ s/$/ -lutil/' Makefile
+sed -i -e '/^LINUX_LIBS/ s/$/ -lutil -lssl -lcrypto -lcurl/' Makefile
