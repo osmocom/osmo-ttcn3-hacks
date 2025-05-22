ed16ac3b25eaaf3a5ddd36b3e607b069dadd132f
Pau Espin Pedrol <pespin@sysmocom.de>
2025-05-22 19:56:11 +0200

    library: NGAP encoding/decoding using libfftranscode
    This allows us to encode/decode 3GPP NGAP messages, as used on the
    N2/NG-C interface control plane between gNB and AMF.
    
    asn files fetched from https://forge.etsi.org/rep/int/5g-core/ngap.git
    directory ttcn/LibNGAP/lib/asn1, commit
    fa43d01e202379a969e991a9454d9c6c6930b5b5.
    
    Change-Id: Ia8e29c77e09a4388c774e3d7286f96052d67fe3f

---
+++ library/ngap/regen_makefile.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+FILES="*.asn *.ttcn NGAP_EncDec.cc"
+
+../../_buildsystem/regen-makefile.sh $FILES
+
+sed -i -e '/^CPPFLAGS/ s/$/ `pkg-config --cflags libfftranscode`/' Makefile
+sed -i -e '/^LDFLAGS/ s/$/ `pkg-config --libs libfftranscode`/' Makefile
+sed -i -e '/^LINUX_LIBS/ s/$/ `pkg-config --libs libfftranscode`/' Makefile
