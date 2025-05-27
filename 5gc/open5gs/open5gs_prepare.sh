68979fbd794c4716437c22a2ffbb66d94934d69b
Oliver Smith <osmith@sysmocom.de>
2025-05-27 11:31:41 +0200

    5gc: testenv: use TESTENV_INSTALL_DIR
    Change-Id: I412e53d132957a877a9a0478df3ac2fe31327a6c

--- 5gc/open5gs/open5gs_prepare.sh
+++ 5gc/open5gs/open5gs_prepare.sh
@@ -1,20 +1,7 @@
 #!/bin/sh -ex
 
-set_ogs_sysconf_dir() {
-	if [ -z "$OSMO_DEV_MAKE_DIR" ]; then
-		# With --binary-repo
-		OGS_INSTALL_DIR="/"
-		OGS_SYSCONF_DIR="$OGS_INSTALL_DIR/etc/"
-	else
-		# Without --binary-repo
-		OGS_INSTALL_DIR="$OSMO_DEV_MAKE_DIR"/..
-		OGS_SYSCONF_DIR="$OGS_INSTALL_DIR/usr/etc/"
-	fi
-}
-
 adjust_sysconfdir_config() {
-	sed -i "s~@sysconfdir@~$1~g" $2
+	sed -i "s~@sysconfdir@~$TESTENV_INSTALL_DIR/etc/~g" $1
 }
 
-set_ogs_sysconf_dir
-adjust_sysconfdir_config "$OGS_SYSCONF_DIR" ./*.yaml
+adjust_sysconfdir_config ./*.yaml
