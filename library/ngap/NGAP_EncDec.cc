06542ed89e0c77dc8dfb40ce6afb4e24a9b72c9e
Pau Espin Pedrol <pespin@sysmocom.de>
2025-06-04 17:00:13 +0200

    WIP
    Change-Id: Id34fb7e4896512d5412a12a4ba7b6adacaf86326

--- library/ngap/NGAP_EncDec.cc
+++ library/ngap/NGAP_EncDec.cc
@@ -61,4 +61,29 @@ NGAP__PDU__Descriptions::NGAP__PDU dec__NGAP__PDU(const OCTETSTRING &stream)
 	return ret_dcc;
 }
 
+OCTETSTRING enc_NGAP_PDUSessionResourceSetupRequestTransfer_IEs(const NGAP__IEs::PDUSessionResourceSetupRequestTransfer &pdu)
+{
+	uint8_t *aper_buf;
+	int aper_buf_len;
+	TTCN_Buffer TTCN_buf;
+	TTCN_buf.clear();
+
+	/* Encode from abstract data type into BER/DER */
+	pdu.encode(NGAP__IEs::PDUSessionResourceSetupRequestTransfer_protocolIEs_descr_, TTCN_buf,
+		   TTCN_EncDec::CT_BER, BER_ENCODE_DER);
+
+	aper_buf_len = fftranscode_ber2aper(FFTRANSC_T_NGAP, &aper_buf, TTCN_buf.get_data(), TTCN_buf.get_len());
+	if (aper_buf_len < 0) {
+		TTCN_error("fftranscode failed.");
+	}
+
+	/* make octetstring from output buffer */
+	OCTETSTRING ret_val(aper_buf_len, aper_buf);
+
+	/* release dynamically-allocated output buffer */
+	fftranscode_free(aper_buf);
+
+	return ret_val;
+}
+
 }
