bb8e3ea1c7b1cd9869509c303f7752e91efc4e08
Philipp Maier <pmaier@sysmocom.de>
2026-09-02 14:05:29 +0200

    eim/prepare.sh: re-align path to restop.py
    The location of restop.py has changed. This patch re-aligns the
    path accordingly.
    
    Related: SYS#8100
    Change-Id: I0625e14d76e230583f1747a01ed554389450a8d1

--- eim/prepare.sh
+++ eim/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 
 # Set mp_restop_path
-RESTOP="$TESTENV_SRC_DIR"/onomondo-eim/contrib/restop.py
+RESTOP="$TESTENV_SRC_DIR"/onomondo-eim/contrib/rest_api_usage_example/restop.py
 test -e "$RESTOP"
 sed \
 	-i \
