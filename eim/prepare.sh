15f51ae4ce5b43753dc9eeb3126204ad48428ff7
Philipp Maier <pmaier@sysmocom.de>
2026-09-07 15:36:21 +0200

    testenv: add alternate config to test JSON ESipa bindings
    The testsuite normally tests the eIM using the ASN.1 ESipa bindings
    (generic). In order to be able to automatically test the JSON ESipa
    bindings as well, let's add a testenv configuration (esipa-json)
    to the testsuite.
    
    Related SYS#8100
    
    Change-Id: Ic5b16d14cc675335356cd25367d44c3f9744b186

--- eim/prepare.sh
+++ eim/prepare.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -ex
+TESTSUITE_CONFIG="$1"
 
 # Set mp_restop_path
 RESTOP="$TESTENV_SRC_DIR"/onomondo-eim/contrib/rest_api_usage_example/restop.py
 test -e "$RESTOP"
+test -e "$TESTSUITE_CONFIG"
 sed \
 	-i \
 	"s,^eIM_Tests\.mp_restop_path := .*,eIM_Tests.mp_restop_path := \"$RESTOP\"," \
-	eIM_Tests.cfg
+	$TESTSUITE_CONFIG
